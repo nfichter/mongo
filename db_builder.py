b672e4a8f3d868c6137929c4135f3215738a8006
JFreyra <efreyra@stuy.edu>
2017-02-04 23:01:09 -0500

    courses code and mark insertion

--- db_builder.py
+++ db_builder.py
@@ -1,22 +1,41 @@
 from pymongo import MongoClient
 
+
 def build():
-    c = MongoClient("lisa.stuy.edu")
-    db = c[""]
+    c = MongoClient()
+    db = c["ada"]
     
     f1 = open("courses.csv","r")
     f2 = open("peeps.csv","r")
 
-    courses = f1.readline()
-    peeps = f2.readline()
+    courses = f1.read().split("\n")
+    peeps = f2.read().split("\n")
 
     students = []
     for line in peeps:
-        lineL = line.split(",")
-        dct = {"name":lineL[0],"age":lineL[1],"id":lineL[2],"courses":[]}
+        if len(line) > 1:
+            lineL = line.split(",")
+            print lineL
+            dct = {"name":lineL[0],"age":lineL[1],"id":lineL[2],"courses":[]}
+            students.append(dct)
     db.students.insert_many(students)
 
     for line in courses:
-        lineL = line.split(",")
-        db.students.find({"id":lineL[2]})
+        if len(line) > 1:
+            lineL = line.split(",")
+            for doc in db.students.find({"id":lineL[2]}):
+                courseL = doc["courses"]
+                courseL.append({"code":lineL[0],"mark":lineL[1]})
+                db.students.update_one(
+                    { "id": lineL[2] },
+                    {
+                        "$set" : { "courses": courseL},
+                    }
+                )
+                print doc
 
+    f1.close()
+    f2.close()
+
+build()
+        
